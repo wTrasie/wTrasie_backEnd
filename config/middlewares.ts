3d2fbd0aaa0694db52e7495c6dce321fed19261f
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2023-03-25 17:44:00 +0100

    Stage (#15)
    * feat: add strapi with basic plugin (#1) (#2)
    
    * Development (#5)
    
    * feat: add strapi with basic plugin (#1)
    
    * Feat/structure (#4)
    
    * fix: delete yarn.lock & update .gitignore
    
    * feat: create content-types
    
    * Development (#8)
    
    * feat: add strapi with basic plugin (#1)
    
    * Feat/structure (#4)
    
    * fix: delete yarn.lock & update .gitignore
    
    * feat: create content-types
    
    * feat: add search engine (#7)
    
    * Development (#11)
    
    * feat: add strapi with basic plugin (#1)
    
    * Feat/structure (#4)
    
    * fix: delete yarn.lock & update .gitignore
    
    * feat: create content-types
    
    * feat: add search engine (#7)
    
    * fix: update strapi (#10)
    
    * Development (#14)
    
    * feat: add strapi with basic plugin (#1)
    
    * Feat/structure (#4)
    
    * fix: delete yarn.lock & update .gitignore
    
    * feat: create content-types
    
    * feat: add search engine (#7)
    
    * Stage (#9)
    
    * feat: add strapi with basic plugin (#1) (#2)
    
    * Development (#5)
    
    * feat: add strapi with basic plugin (#1)
    
    * Feat/structure (#4)
    
    * fix: delete yarn.lock & update .gitignore
    
    * feat: create content-types
    
    * Development (#8)
    
    * feat: add strapi with basic plugin (#1)
    
    * Feat/structure (#4)
    
    * fix: delete yarn.lock & update .gitignore
    
    * feat: create content-types
    
    * feat: add search engine (#7)
    
    * fix: update strapi (#10)
    
    * Stage (#12)
    
    * feat: add strapi with basic plugin (#1) (#2)
    
    * Development (#5)
    
    * feat: add strapi with basic plugin (#1)
    
    * Feat/structure (#4)
    
    * fix: delete yarn.lock & update .gitignore
    
    * feat: create content-types
    
    * Development (#8)
    
    * feat: add strapi with basic plugin (#1)
    
    * Feat/structure (#4)
    
    * fix: delete yarn.lock & update .gitignore
    
    * feat: create content-types
    
    * feat: add search engine (#7)
    
    * Development (#11)
    
    * feat: add strapi with basic plugin (#1)
    
    * Feat/structure (#4)
    
    * fix: delete yarn.lock & update .gitignore
    
    * feat: create content-types
    
    * feat: add search engine (#7)
    
    * fix: update strapi (#10)
    
    * fix: delete package-json
    
    * Feat/update strapi (#13)
    
    * fix: update strapi
    
    * feat: add modles && update strapi

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,24 @@
 export default [
-  'strapi::errors',
-  'strapi::security',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://wtrasiepl.s3.eu-west-1.amazonaws.com",
+          ],
+          "media-src": ["'self'", "data:", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
